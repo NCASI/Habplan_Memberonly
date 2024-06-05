9c03e1a39e156986ee0c1c2bb94ae2cc27244483
maxdoltonjones2 <153532476+maxdoltonjones2@users.noreply.github.com>
2024-06-05 10:26:20 -0400

    Update FlowPlot_func_v1.R

--- HabplanR/R/FlowPlot_func_v1.R
+++ HabplanR/R/FlowPlot_func_v1.R
@@ -47,6 +47,7 @@ flowPlot <- function(flow.data, nyear){
     #geom_hline(yintercept = (target+th.hi), linetype = 2, color = "black") +
     #geom_hline(yintercept = (target-th.lo), linetype = 2, color = "black") +
     #scale_color_viridis_d() +
+    scale_x_continuous(expand = c(0, 0)) +
     ggtitle("Flow output over time") +
     xlab("Year") +
     ylab("Flow") +
